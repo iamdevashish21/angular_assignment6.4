4c48c202c2ba23edf154feadc0c25e60325e2043
devashishpotnis <devashishpotnis@gmail.com>
2024-11-17 02:07:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewcompComponent } from './newcomp/newcomp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NewcompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = '4';
+
 }
